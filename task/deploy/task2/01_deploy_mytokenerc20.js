3a5e229e64a9b83adfd78a3681f39f87d195d8da
gin <2440016523@qq.com>
2025-08-20 21:07:22 +0800

    task2-部署增加合约验证

--- task/deploy/task2/01_deploy_mytokenerc20.js
+++ task/deploy/task2/01_deploy_mytokenerc20.js
@@ -1,11 +1,17 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const account1 = (await getNamedAccounts()).account1
     const { deploy } = deployments
-    await deploy("MyTokenERC20", {
+    const myToken = await deploy("MyTokenERC20", {
         from: account1,
         args: ["MyToken", "MT"],
         log: true,
     })
+    if (hre.network.config.chainId == 11155111) {
+        await hre.run("verify:verify", {
+            address: myToken.address,
+            constructorArguments: ["MyToken", "MT"],
+        });
+    }
 }
 
 module.exports.tags = ["MyTokenERC20"]
