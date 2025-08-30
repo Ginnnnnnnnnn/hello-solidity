4bbf4c53223aef9e9d3b8f6d6734072b1a95c6fb
gin <2440016523@qq.com>
2025-08-30 23:57:44 +0800

    task swap

---
+++ task-swap/hardhat.config.js
@@ -0,0 +1,37 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require('hardhat-deploy')
+require("hardhat-deploy-ethers")
+
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY_1, process.env.PRIVATE_KEY_2]
+    },
+  },
+  namedAccounts: {
+    owner: {
+      default: 0
+    },
+    account1: {
+      default: 1
+    }
+  },
+  gasReporter: {
+    enabled: false
+  }
+};
